445e096441c3662d2f92132ebfdca15569f016f4
hadeerKarem <hadeer.karem95@gmail.com>
2016-01-07 17:47:40 +0200

    Implemented Shortest Path

---
+++ src/de/vogella/algorithms/dijkstra/engine/DijkstraAlgorithm.java
@@ -0,0 +1,6 @@
+package de.vogella.algorithms.dijkstra.engine;
+
+
+public class DijkstraAlgorithm {
+
+}
--- src/socialNetwork.java
+++ src/socialNetwork.java
@@ -6,11 +6,15 @@ public class socialNetwork {
     public static void main(String [] args)throws IOException{
         Network net = new Network();
 
-        ArrayList<String> userf = new ArrayList<String>();
+       // ArrayList<String> userf = new ArrayList<String>();
 
-        userf = net.searchByName("Muhammad Salah").friendList;
+        //userf = net.searchByName("Muhammad Salah").friendList;
 
-        System.out.print(userf);
+        //System.out.print(userf);
+        ArrayList<String> path = new ArrayList<String>();
+        
+        path = net.shortestPath("Muhammad Salah", "Nada Diaa");
+        System.out.print(path);
 
     }
 }
